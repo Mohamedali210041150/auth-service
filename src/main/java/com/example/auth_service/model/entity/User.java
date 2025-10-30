777e2c68f296d82d4694908f9be793f9ab1fa1da
210041272 <142823422+abdulmajeed-alqadami@users.noreply.github.com>
2025-10-30 21:07:39 +0600

    Update User.java
    Added the  getter method getId() and setId() methods to the User entity

--- src/main/java/com/example/auth_service/model/entity/User.java
+++ src/main/java/com/example/auth_service/model/entity/User.java
@@ -39,6 +39,8 @@ public class User {
     }
 
     // Getters & Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
     
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
